3491def8a25aaeab943e978e32bc0e033e51b308
aromano272 <andre.romano272@gmail.com>
2014-10-15 04:39:50 +0100

    added directive for new-item, getting new item to save to json working

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,70 @@
 var app = angular.module("app", []);
 
 
-app.controller("HeaderHolderController", ["$http", function($http) {
+app.controller("HeaderHolderController", ["$scope", "$http", function($scope, $http) {
   var todoList = this;
 
   $http.get("/app/todo.json").success(function(data) {
     todoList.headers = data;
   });
 
-  
+    
 
 
 }]);
+
+app.controller("NewItemController", ["$scope", "$http", function($scope, $http) {
+
+  $scope.newTasks = [{"item": "item 0"}, {"item": "item 1"}];
+
+  $scope.save = function() {
+    header = $scope.header;
+    description = $scope.description;
+    tasks = $scope.newTasks;
+
+    console.log(header + description + tasks);
+
+    // create new obj
+    var newItem = {};
+    // push values to that obj
+    newItem.headerTitle = header;
+    newItem.description = description;
+    newItem.tasks = tasks;
+    // push object to json file
+    var dataPost = $http.push("/app/todo.json", newItem);
+    dataPost.success(function(data, status, headers, config) {
+      $scope.message = data;
+    });
+  }
+
+  $(document).on("keydown", ".item-holder__new-tasks input:last-child", function() {
+    $scope.newTasks.push({"item": "item " + $scope.newTasks.length });
+    $scope.$apply();
+  });
+
+
+}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.directive("addNewItem", function() {
+  return {
+    restrict: "E",
+    templateUrl: "new-item.html"
+  };
+});
 
 
 
